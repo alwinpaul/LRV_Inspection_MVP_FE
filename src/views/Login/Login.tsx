f249be7ed687bf8c82764c126cfc9f332f3da99a
Alwin Paul <alwin.paul7489@gmail.com>
2024-04-27 22:06:20 -0400

    login style update

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -22,16 +22,16 @@ export default function Login() {
     return (
 
         <section className="w-full mt-20 flex justify-center">
-            <div className='w-full sm:w-4/12 sm:p-5 flex items-center bg-blue-100 border-2 border-blue-300 rounded-lg shadow-md'>
+            <div className='w-full sm:w-4/12 sm:p-5 flex items-center bg-blue-50 border-2 border-blue-300 rounded-lg shadow-md'>
                 <form className="sic-box p-3 w-full" onSubmit={handleLogin}>
                     <div className="sic-field sm:flex items-center justify-start my-5">
-                        <div className="label sm:w-1/3 text-left">Email : </div>
+                        <div className="label sm:w-1/3 text-left text-xs sm:text-sm">Username : </div>
                         <div className="inputf w-full sm:w-2/3">
                             <input type="text" name="username" id="username" onChange={handleUsernameChange} className='border border-gray-900/50 rounded-md w-full h-10 p-2' />
                         </div>
                     </div>
                     <div className="sic-field sm:flex items-center justify-start my-5">
-                        <div className="label sm:w-1/3 text-left">Password : </div>
+                        <div className="label sm:w-1/3 text-left text-xs sm:text-sm">Password : </div>
                         <div className="inputf w-full sm:w-2/3">
                             <input type="password" name="password" id="password" onChange={handlePasswordChange} className='border border-gray-900/50 rounded-md w-full h-10 p-2' />
                         </div>
