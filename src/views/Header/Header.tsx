0f9ea1790aec45a8391e70c0864d1f514c349d86
Alwin Paul <alwin.paul7489@gmail.com>
2024-04-27 22:13:43 -0400

    update header shadow

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -3,7 +3,7 @@ import logo from "./../../assets/KEOLIS_R.webp"
 const Header = () => {
     return (
         <div className="h-6 sm:h-[60px]">
-            <div className="mb-8 sm:mb-3 bg-keolis py-3 fixed left-0 top-0 z-10">
+            <div className="mb-8 sm:mb-3 bg-keolis py-3 fixed left-0 top-0 z-10 shadow-lg">
                 <img className="w-4/12 sm:w-1/12" src={logo} alt="client-logo" />
             </div>
         </div>
