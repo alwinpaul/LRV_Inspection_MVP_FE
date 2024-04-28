7104723a1034339c141610e44917e80c9e84640d
Alwin Paul <alwin.paul7489@gmail.com>
2024-04-27 21:07:06 -0400

    review page updated for mobile view

--- src/views/Review/Review.tsx
+++ src/views/Review/Review.tsx
@@ -112,13 +112,13 @@ const Review = () => {
 
     return (
         <section>
-            <div className="w-10/12 m-auto my-10">
+            <div className="w-11/12 sm:w-10/12 m-auto my-10">
                 <VehicleInfoView />
             </div>
 
 
-            <div className="flex justify-center w-full">
-                <div className="w-5/12 mx-3">
+            <div className="flex flex-col sm:flex-row justify-center w-full">
+                <div className="w-11/12 m-auto sm:w-5/12 mx-3">
                     <div className="my-5">
                         <Table dataSource={cabData} columns={cabColumns} pagination={false} rowKey='id' />
                     </div>
@@ -126,7 +126,7 @@ const Review = () => {
                         <Table dataSource={dddData} columns={dddColumns} pagination={false} rowKey='id' />
                     </div>
                 </div>
-                <div className="w-5/12 mx-3">
+                <div className="w-11/12 m-auto sm:w-5/12 mx-3">
                     <div className="my-5">
                         <Table dataSource={psgData} columns={psgColumns} pagination={false} rowKey='id' />
                     </div>
@@ -136,28 +136,29 @@ const Review = () => {
                 </div>
             </div>
 
-            <div className="flex items-center justify-center w-10/12 m-auto">
+            <div className="flex flex-col sm:flex-row sm:items-center justify-center w-full sm:w-10/12 m-auto">
                 {exteriorOptionalData.map(opData => (
-                    <div className="border border-slate-300 w-3/12 h-22">
-                        <div className="h-10 font-bold text-sm m-2">{opData.label}</div>
-                        <div className="m-2">{showCrossCheck(opData.value)}</div>
+                    <div className="flex items-center sm:block border border-slate-300 sm:w-3/12 h-22 sm:p-2">
+                        < div className="sm:h-10 font-bold text-sm m-2 w-9/12 sm:w-full text-left sm:text-center p-2" > {opData.label}</div>
+                        <div className="m-2 w-3/12 sm:w-full">{showCrossCheck(opData.value)}</div>
                     </div>
-                ))}
-            </div>
+                ))
+                }
+            </div >
 
-            <div className="flex items-start justify-start w-10/12 m-auto my-5">
+            <div className="flex flex-col items-start justify-start w-full sm:w-10/12 m-auto my-5">
                 <div className="font-bold text-sm m-2 w-2/12 text-left">Notes:</div>
-                <div className="m-2 text-left w-10/12">{notes}</div>
+                <div className="m-2 text-left w-full sm:w-10/12">{notes}</div>
             </div>
 
-            <div className="flex items-center justify-start w-10/12 m-auto my-5">
+            <div className="flex items-center justify-start w-full sm:w-10/12 m-auto my-5">
                 <div className="font-bold text-sm m-2 w-2/12 text-left">Mileage:</div>
                 <div className="m-2 text-left w-10/12">{mileage}</div>
             </div>
 
-            <div className="flex items-center justify-start w-10/12 m-auto my-5">
-                <div className="font-bold text-sm m-2 w-2/12 text-left">Fit for service:</div>
-                <div className="m-2 text-left w-10/12">{showCrossCheck(fitForService)}</div>
+            <div className="flex items-center justify-start w-full sm:w-10/12 m-auto my-5">
+                <div className="font-bold text-sm m-2 sm:w-2/12 text-left">Fit for service:</div>
+                <div className="m-2 text-left sm:w-10/12">{showCrossCheck(fitForService)}</div>
             </div>
 
             <div className="py-5 bg-[#f7f7f7] w-full z-10">
@@ -165,7 +166,7 @@ const Review = () => {
                 <Button type="primary" size="large" className="px-3 sm:px-20 mx-3">Submit</Button>
             </div>
 
-        </section>
+        </section >
 
     )
 }
